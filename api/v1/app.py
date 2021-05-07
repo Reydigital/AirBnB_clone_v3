763cd7a9c921ecee4df0c92d4bbbae60381a96c2
ThibaudP <t.poncin@gmail.com>
2021-05-07 18:15:17 +0000

    feat: add CORS support (task 13)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,14 @@
 """
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
