b006d7401b6883754d1f90c4726f4e4961264924
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-05-07 08:35:23 +0000

    Add task 9

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,61 @@
+
+
+#!/usr/bin/python3
+"""
+    This is the amenities page handler for Flask.
+"""
+from api.v1.views import app_views
+from models import storage
+from flask import abort, jsonify, request
+
+from models.amenity import Amenity
+
+amnt_m = ['GET', 'POST']
+
+
+@app_views.route('/amenities', methods=amnt_m, strict_slashes=False)
+def amenities():
+    """
+        Flask route at /amenities.
+    """
+    if request.method == 'POST':
+        try:
+            kwargs = request.get_json()
+        except:
+            return {"error": "Not a JSON"}, 400
+        if "name" not in kwargs:
+            return {"error": "Missing name"}, 400
+        new_amnt = Amenity(**kwargs)
+        storage.new(new_amnt)
+        storage.save()
+        return new_amnt.to_dict(), 201
+
+    elif request.method == 'GET':
+        return jsonify([a.to_dict() for a in storage.all("Amenity").values()])
+
+amnt_id_m = ['GET', 'DELETE', 'PUT']
+
+
+@app_views.route('/amenities/<id>', methods=amnt_id_m, strict_slashes=False)
+def amenities_id(id):
+    """
+        Flask route at /amenities/<id>.
+    """
+    amnt = storage.get(Amenity, id)
+    if (amnt):
+        if request.method == 'DELETE':
+            storage.delete(amnt)
+            storage.save()
+            return {}, 200
+
+        elif request.method == 'PUT':
+            try:
+                kwargs = request.get_json()
+            except:
+                return {"error": "Not a JSON"}, 400
+            for k, v in kwargs.items():
+                if k not in ["id", "created_at", "updated_at"]:
+                    setattr(amnt, k, v)
+            storage.save()
+        return amnt.to_dict()
+    abort(404)
